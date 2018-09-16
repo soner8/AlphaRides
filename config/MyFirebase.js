53a1a139ee4961165453f4dc6cfc4ef8c73e3cc5
Yingi Kem <yingikeme@yahoo.com>
2018-09-16 14:29:26 -0700

    Changed Firebase API Keys

--- config/MyFirebase.js
+++ config/MyFirebase.js
@@ -1,12 +1,12 @@
 import Firebase from 'firebase';
 
 let config = {
-    apiKey: "AIzaSyA_ljlUDYVJJ5uHKbhNmWK8O6Rs5bS_-5I",
-    authDomain: "alpharides-b2c35.firebaseapp.com",
-    databaseURL: "https://alpharides-b2c35.firebaseio.com/",
-    projectId: "alpharides-b2c35",
-    storageBucket: "alpharides-b2c35.appspot.com",
-    messagingSenderId: "654672384859"
+    apiKey: "AIzaSyCXZiERpF2DscCApxbRca9Y7UtglIdc-G8",
+    authDomain: "alpharides-f115c.firebaseapp.com",
+    databaseURL: "https://alpharides-f115c.firebaseio.com/",
+    projectId: "alpharides-f115c",
+    storageBucket: "alpharides-f115c.appspot.com",
+    messagingSenderId: "38995523207"
 };
 
 
