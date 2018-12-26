cd788a6fe7d349468a24e658fa2ca8ac3fb2222d
Yingi Kem <yingikeme@yahoo.com>
2018-12-26 01:56:51 -0800

    Added Pic as Activity Indicator

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import UserReducer from './UserReducer';
 import { db } from "./config/MyFirebase";
+import Spinner from 'react-native-spinkit';
 import firebase from 'react-native-firebase';
+import { Image, Alert, View } from 'react-native'
 
 // Redux Store for User
 const store = createStore(UserReducer);
@@ -30,7 +32,7 @@ export default class App extends Component {
   onAuthStateChanged = (user) => {
     console.log("authenticated Now")
     console.log(user)
-    this.setState({ isAuthenticationReady: true });
+    //this.setState({ isAuthenticationReady: true });
     this.setState({ isAuthenticated: !!user });
     this.unsubscriber();
     console.log('Zaza')
@@ -49,7 +51,52 @@ export default class App extends Component {
     console.log('It is Showing')
     // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
     if (!this.state.isLoadingComplete && !this.state.isAuthenticationReady) {
-      return null;
+      return (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0, 0.9)',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <View
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(0,0,0, 0.9)'
+            }}
+          >
+            <Image
+              style={{
+                backgroundColor: '#ccc',
+                flex: 1,
+                resizeMode: 'contain',
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+              }}
+              source={require('./app/images/BertaCabs.jpg')}
+            />
+          </View>
+          <Spinner
+            style={{
+              marginBottom: 50
+            }}
+            isVisible={true}
+            size={150}
+            type={'Bounce'}
+            color={'#faebd7'}
+
+          />
+        </View>
+
+      );
+      //return null;
       //OR
       // return <ActivityIndicator />
     }
